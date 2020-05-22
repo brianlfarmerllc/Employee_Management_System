11abe72b134d801b5cf39c3611f63d772bc3df42
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-05-21 19:34:42 -0500

    create connection to db, create table library with table classes

--- cmsSeeds.sql
+++ cmsSeeds.sql
@@ -3,21 +3,24 @@ CREATE DATABASE employee_tracker_db;
 USE employee_tracker_db;
 
 CREATE TABLE department (
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(30)
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30),
+    PRIMARY KEY (id),
 );
 
 CREATE TABLE role (
-    id INTEGER PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT NOT NULL,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INTEGER
+    department_id INTEGER,
+    PRIMARY KEY (id)
 )
 
 CREATE table employee (
-    id INTEGER PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30),
     last_name VARCHAR (30),
     role_id INTEGER,
-    manager_id INTEGER
+    manager_id INTEGER,
+    PRIMARY KEY (id)
 )
